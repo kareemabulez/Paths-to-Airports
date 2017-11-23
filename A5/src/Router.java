f230dc80516d6044ffed2c3a860603aeb6bf4d77
andrewgodbout <agodbout@upei.ca>
2017-11-23 17:45:19 -0400

    first commit

---
+++ A5/src/Router.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+//is it possible to get from start to end
+public class Router {
+
+    public Router (Map<String, City> database) {
+        //probably save that database off as a class member variabl
+    }
+
+    //search to find a route from start to finish in maxSteps or less steps
+    public Iterable <String> route(String start, String finish, int maxSteps) { 
+
+        /* Implement this method and any other method that might help in the task of finding a route from start to finish */
+
+        return null; //hopefully you'll return some Iterable object like an ArrayList for example with the names of cities leading from start to finish
+    }
+}
