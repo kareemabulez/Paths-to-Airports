65c4bcc3659868a8a18c9230c1c15276a63a8718
Kareem Abulez <kareemabulez1@gmail.com>
2017-12-03 21:40:29 -0400

    finished assignment

--- A5/src/Router.java
+++ A5/src/Router.java
@@ -1,17 +1,40 @@
 import java.util.Map;
+import java.util.*;
 
 //is it possible to get from start to end
 public class Router {
 
+    public Map<String, City> database;
+    ArrayList<String> listOfRoutes = new ArrayList<>();
+    int numOfSteps = 0;
+
     public Router (Map<String, City> database) {
-        //probably save that database off as a class member variabl
+        this.database = database;
+        //probably save that database off as a class member variable
     }
 
     //search to find a route from start to finish in maxSteps or less steps
-    public Iterable <String> route(String start, String finish, int maxSteps) { 
+    public Iterable <String> route(String start, String finish, int maxSteps) {
+        DFS(start, finish, maxSteps, listOfRoutes);
+        return listOfRoutes;
+    }
 
-        /* Implement this method and any other method that might help in the task of finding a route from start to finish */
+    public void DFS(String start, String finish, int maxSteps, ArrayList<String> listOfRoutes) {
+        numOfSteps++;
+        listOfRoutes.add(start);
 
-        return null; //hopefully you'll return some Iterable object like an ArrayList for example with the names of cities leading from start to finish
+        if (listOfRoutes.size() > maxSteps) { return; }
+
+        if (start.equals(finish)) { return; }
+
+        for (City city : database.get(start).getNeighbours()) {
+            if (!(listOfRoutes.contains(finish))) {
+                DFS(city.getName(), finish, maxSteps, listOfRoutes);
+            }
+            listOfRoutes.remove(listOfRoutes.size() - 1);
+            numOfSteps--;
+            return;
+        }
     }
 }
+
